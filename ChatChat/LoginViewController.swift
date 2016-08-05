6403f0c62a4362b711a9d8125ee329b5b1564fed
Tom Elliott <tomelliott@fb.com>
2016-08-05 22:26:32 +0100

    End of tutorial as it currently stands, but ported to latest version of Firebase

--- ChatChat/LoginViewController.swift
+++ ChatChat/LoginViewController.swift
@@ -21,14 +21,68 @@
 */
 
 import UIKit
+import Firebase
 
-class LoginViewController: UIViewController {
+class LoginViewController: UIViewController, UITextFieldDelegate {
+  
+  // MARK: Properties
+  var user: FIRUser?
   
   @IBOutlet weak var nameField: UITextField!
   @IBOutlet weak var bottomLayoutGuideConstraint: NSLayoutConstraint!
+
+  // MARK: View Lifecycle
   
-  @IBAction func loginDidTouch(_ sender: AnyObject) {
+  override func viewWillAppear(_ animated: Bool) {
+    super.viewWillAppear(animated)
+    NotificationCenter.default.addObserver(self, selector: #selector(self.keyboardWillShowNotification(_:)), name: NSNotification.Name.UIKeyboardWillShow, object: nil)
+    NotificationCenter.default.addObserver(self, selector: #selector(self.keyboardWillHideNotification(_:)), name: NSNotification.Name.UIKeyboardWillHide, object: nil)
   }
   
+  override func viewWillDisappear(_ animated: Bool) {
+    super.viewWillDisappear(animated)
+    NotificationCenter.default.removeObserver(self, name: NSNotification.Name.UIKeyboardWillShow, object: nil)
+    NotificationCenter.default.removeObserver(self, name: NSNotification.Name.UIKeyboardWillHide, object: nil)
+  }
+  
+  // MARK: Actions
+  
+  @IBAction func loginDidTouch(_ sender: AnyObject) {
+    if nameField?.text != "" {
+      FIRAuth.auth()?.signInAnonymously(completion: { (user, error) in
+        if let err = error {
+          print(err.description)
+          return
+        }
+        
+        self.user = user
+        self.performSegue(withIdentifier: "LoginToChat", sender: nil)
+      })
+    }
+  }
+  
+  // MARK: Navigation
+  
+  override func prepare(for segue: UIStoryboardSegue, sender: AnyObject?) {
+    super.prepare(for: segue, sender: sender)
+    let navVc = segue.destinationViewController as! UINavigationController
+    let channelVc = navVc.viewControllers.first as! ChannelListViewController
+    
+    channelVc.senderId = user?  .uid
+    channelVc.senderDisplayName = nameField?.text
+  }
+  
+  // MARK: - Notifications
+  
+  func keyboardWillShowNotification(_ notification: Notification) {
+    let keyboardEndFrame = ((notification as NSNotification).userInfo![UIKeyboardFrameEndUserInfoKey] as! NSValue).cgRectValue()
+    let convertedKeyboardEndFrame = view.convert(keyboardEndFrame, from: view.window)
+    bottomLayoutGuideConstraint.constant = view.bounds.maxY - convertedKeyboardEndFrame.minY
+  }
+  
+  func keyboardWillHideNotification(_ notification: Notification) {
+    bottomLayoutGuideConstraint.constant = 48
+  }
+
 }
 
